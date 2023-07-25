8c8f6f39150d0b08693df3019f62f11fce290d26
dell <vidhyaanand030@gmail.com>
2023-07-25 06:56:42 -0700

    Echo command and color codes

---
+++ print.sh
@@ -0,0 +1,18 @@
+echo helloworld
+echo "<<<<<<<<<Hello world>>>>>>>>>>"
+
+#Color codes supported by shell scripting :
+#Red        31
+#Green      32
+#Yellow     33
+#Blue       34
+#Magenta    35
+#Cyan       36
+
+#Syntax : echo -e "\e[codemMessage\e[0m"
+echo -e "\e[31mHello Vidhya\e[0m"
+echo -e "\e[32mHello Vidhya\e[0m"
+echo -e "\e[33mHello Vidhya\e[0m"
+echo -e "\e[34mHello Vidhya\e[0m"
+echo -e "\e[35mHello Vidhya\e[0m"
+echo -e "\e[36mHello Vidhya\e[0m"
